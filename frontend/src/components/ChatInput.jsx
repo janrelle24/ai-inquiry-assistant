0dd1803e7d0c5db99253ac28eddf14a5c2a79c41
janrelle24 <lubianojanrelle@gmail.com>
2026-07-14 14:16:53 +0800

    improve the button into send icon

--- frontend/src/components/ChatInput.jsx
+++ frontend/src/components/ChatInput.jsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
+import { SendHorizontal } from "lucide-react";
 
 function ChatInput({onSend}) {
     const [input, setInput] = useState("");
     
+    const handleKeyDown = (e) => {
+        if(e.key === "Enter" && !e.shiftKey){
+            e.preventDefault();
+            if(!input.trim()) return;
+
+            onSend(input);
+            setInput("");
+        }
+    }
     const handleSubmit = (e) => {
         e.preventDefault();
 
@@ -15,22 +25,24 @@ function ChatInput({onSend}) {
 
     return (
         <form
-        onSubmit={handleSubmit}
-        className="border-t bg-white p-4"
+            onSubmit={handleSubmit}
+            className="border-t bg-white p-4"
         >
-        <div className="flex gap-3">
-            <input
-            type="text"
-            placeholder="Ask a medical question..."
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            className="flex-1 rounded-xl border p-3 outline-none focus:border-blue-600"
+        <div className="flex items-end gap-3 rounded-2xl border bg-white p-3 shadow-sm">
+            <textarea
+                type="text"
+                placeholder="Ask a medical question..."
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                onKeyDown={handleKeyDown}
+                className="flex-1 resize-none border-none outline-none"
             />
 
             <button
-            className="rounded-xl bg-blue-600 px-6 text-white"
+                type="submit"
+                className="rounded-xl bg-blue-600 p-3 text-white transition hover:bg-blue-700 hover:cursor-pointer"
             >
-            Send
+                <SendHorizontal size={20} />
             </button>
         </div>
         </form>
